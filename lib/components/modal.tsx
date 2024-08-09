5c4214651e5592646844d4c7b4695113f731ddc9
nmiller44 <neildmiller@gmail.com>
2024-08-08 21:47:59 -0500

    v0.0.29 Modal Size Fix

--- lib/components/modal.tsx
+++ lib/components/modal.tsx
@@ -38,7 +38,7 @@ export const Modal = (props: ModalProps) => {
 
 
                 <div className="fixed inset-0 z-10 overflow-y-auto">
-                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                    <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
                     <Transition.Child
                         as={Fragment}
                         enter="ease-out duration-300"
@@ -48,7 +48,7 @@ export const Modal = (props: ModalProps) => {
                         leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                         leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     >
-                        <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                        <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-full max-w-lg">
                             { children } 
 
                             { dismiss &&
