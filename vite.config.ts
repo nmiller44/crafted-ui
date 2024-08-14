7ac40cca8421a845631a7db2824a9af134e9d35d
Neil Miller <neildmiller@gmail.com>
2024-08-13 21:13:53 -0500

    Ignore Storybook

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
+import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    dts({ include: ['lib'] })
+    dts({ 
+      include: ['lib'],
+      exclude: ['lib/**/*.stories.tsx'] 
+    })
   ],
   build: {
     outDir: resolve(__dirname, 'dist'),
