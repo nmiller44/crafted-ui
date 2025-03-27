417a2748a4f1838042790c5f288b4f82da076b66
Neil Miller <neildmiller@gmail.com>
2025-03-26 21:10:47 -0500

    Card Modernization

--- lib/components/card.tsx
+++ lib/components/card.tsx
@@ -1,77 +1,75 @@
 import { classNames } from "../utils.js";
+import { Heading5 } from "./heading.js";
 
 export type CardProps = {
     title?: string;
-    showTitle?: boolean;
-    children: React.ReactNode;
+    subtitle?: string;
+    className?: string;
+    children?: React.ReactNode;
+}
+
+export const Card = ({ title, subtitle, className, children }: CardProps) => {
+
+    return (
+        <div className={classNames( 
+                                "flex flex-col",
+                                "rounded-md border border-border bg-card text-card-foreground shadow",
+                                className)}>
+            { !!title
+                ? <CardHeader title={ title } subtitle={ subtitle } />
+                : children
+            }
+        </div>
+    )
 }
 
 export type CardHeaderProps = {
     title?: string;
     subtitle?: string;
+    className?: string;
     children?: React.ReactNode;
+}
+
+export const CardHeader = ({ title, subtitle, className, children }: CardHeaderProps) => {
+
+    return (
+        <div className={classNames(
+                            "px-5 py-6 border-b border-border",
+                            className)}>
+            { !!title 
+                ? <Heading5 title={ title } subtitle={ subtitle } />
+                : children 
+            }
+        </div>
+    )
 }
 
 export type CardBodyProps = {
+    className?: string;
     children?: React.ReactNode;
+}
+
+export const CardBody = ({ className, children }: CardBodyProps) => {
+
+    return (
+        <div className={classNames("p-5 grow", className)}>
+            { children }
+        </div>
+    )
 }
 
 export type CardFooterProps = {
+    className?: string;
     children?: React.ReactNode;
 }
 
-export const Card = (props: CardProps) => {
-
-    const title = props.title;
-    const showTitle = props.showTitle || !!title || false;
-    const children = props.children
-
-
-    return (
-        <div className="rounded-md border border-neutral-200 bg-white shadow shadow-neutral-300 flex flex-col">
-            {
-                showTitle &&
-                <div className="px-5 py-6 border-b border-neutral-200">
-                    <div className="text-lg font-medium leading-6 text-neutral-900">{ title }</div>
-                    <div></div>
-                </div>
-            }
-            { children }
-        </div>
-    )
-}
-
-export const CardHeader = (props: CardHeaderProps) => {
-    const children = props.children;
+export const CardFooter = ({ className, children }: CardFooterProps) => {
 
     return (
         <div className={classNames(
-            "px-5 py-6 border-b border-neutral-200",
-            "text-lg font-medium leading-6 text-neutral-900"
-            )}>
-            { children }
-        </div>
-    )
-}
-
-export const CardBody = (props: CardBodyProps) => {
-    const children = props.children;
-
-    return (
-        <div className="p-5 grow">
-            { children }
-        </div>
-    )
-}
-
-export const CardFooter = (props: CardFooterProps) => {
-    const children = props.children;
-
-    return (
-        <div className={classNames(
-            "px-5 py-6 border-t border-neutral-200",
-            "text-neutral-900"
-            )}>
+                            "px-5 py-6 border-t border-border",
+                            "text-foreground",
+                            className)}>
             { children }
         </div>
     )
