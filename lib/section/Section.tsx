b864451f410239aa13fa6a42f8e3f8fe68c6633b
Neil Miller <neildmiller@gmail.com>
2025-04-20 21:31:24 -0500

    Section w Header

--- lib/section/Section.tsx
+++ lib/section/Section.tsx
@@ -1,11 +1,18 @@
 import { classNames } from "~/utils";
+import { SectionHeader } from "./SectionHeader";
 
-export type SectionProps = React.ComponentProps<"section">
+export type SectionProps = React.ComponentProps<"section"> & {
+    title?: string;
+    subtitle?: string;
+}
 
-export const Section = ({ className, children, ...props }: SectionProps) => {
+export const Section = ({ title, subtitle, className, children, ...props }: SectionProps) => {
 
     return (
         <section className={classNames("[&:not(:first-of-type)]:mt-12", className)} { ...props }>
+            { !!title &&
+                <SectionHeader title={ title } subtitle={ subtitle } />
+            }
             { children }
         </section>
     )
