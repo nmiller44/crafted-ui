bd461f7b66672cacaa2a844b4903622a8830e0fe
Neil Miller <neildmiller@gmail.com>
2025-05-11 18:02:01 -0500

    Tablist handle not-group background

--- lib/tablist/tablist.stories.tsx
+++ lib/tablist/tablist.stories.tsx
@@ -16,8 +16,22 @@ export const Basic: Story = {
   args: {},
   render: (args) => (
     <div className="flex flex-col space-y-12">
+        <div className="flex flex-col space-y-12 px-4 py-2">
+            <Tablist bordered>
+                <TablistItem><div className="active">Tab 1</div></TablistItem>
+                <TablistItem><a href="#">Tab 2</a></TablistItem>
+                <TablistItem><a href="#">Tab 3</a></TablistItem>
+            </Tablist>
+        </div>
         <div className="flex flex-col space-y-12 px-4 py-2 group bg-background">
             <Tablist bordered>
+                <TablistItem><div className="active">Tab 1</div></TablistItem>
+                <TablistItem><a href="#">Tab 2</a></TablistItem>
+                <TablistItem><a href="#">Tab 3</a></TablistItem>
+            </Tablist>
+        </div>
+        <div className="flex flex-col space-y-12 px-4 py-2">
+            <Tablist>
                 <TablistItem><div className="active">Tab 1</div></TablistItem>
                 <TablistItem><a href="#">Tab 2</a></TablistItem>
                 <TablistItem><a href="#">Tab 3</a></TablistItem>
