fa8ac46d9ac80cc6ee897a663dc4e241c96fa339
Neil Miller <neildmiller@gmail.com>
2025-06-19 23:05:45 -0500

    Page Title inline

--- lib/page/Page.tsx
+++ lib/page/Page.tsx
@@ -1,11 +1,18 @@
 import { classNames } from "~/utils";
+import { PageHeader } from "./PageHeader";
 
-export type PageProps = React.ComponentProps<"div">
+export type PageProps = React.ComponentProps<"div"> & {
+    title?: string,
+    subtitle?: string,
+}
 
-export const Page = ({ className, children, ...props }: PageProps) => {
+export const Page = ({ title, subtitle, className, children, ...props }: PageProps) => {
 
     return (
         <div className={classNames("max-w-container mx-auto px-4 py-6 sm:px-6 lg:px-8 lg:py-8", className)} { ...props }>
+            { !!title &&
+                <PageHeader title={ title } subtitle={ subtitle } />
+            }
             { children }
         </div>
     )
