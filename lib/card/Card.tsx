8924cce3975e14eb261ee39f830d1bd53a275d34
Neil Miller <neildmiller@gmail.com>
2025-12-06 18:44:21 -0600

    Styling cleanup

--- lib/card/Card.tsx
+++ lib/card/Card.tsx
@@ -4,20 +4,27 @@ import { classNames } from "~/utils";
 export type CardProps = React.ComponentProps<"div"> & {
     title?: string;
     subtitle?: string;
-    body?: boolean
+    body?: boolean;
+    full?: boolean;
 }
 
-export const Card = ({ title, subtitle, body = false, className, children, ...props }: CardProps) => {
+export const Card = ({ title, subtitle, body = false, full = false, className, children, ...props }: CardProps) => {
 
     return (
         <div className={classNames( 
                                 "flex flex-col",
-                                "rounded-md border border-border bg-card text-card-foreground shadow",
+                                "bg-card text-card-foreground",
+                                full    ? "md:rounded-md md:border md:border-border md:shadow"
+                                        : "rounded-md border border-border shadow",
                                 className)} { ...props }>
             { !!title &&
                 <CardHeader title={ title } subtitle={ subtitle } />
             }
-            <div className={classNames(body ? "px-8 py-10 space-y-6" : "")}>
+            <div className={classNames(
+                                "space-y-6",
+                                body    ? full ? "p-0 md:px-8 md:py-10" :  "px-8 py-10"
+                                        : ""
+                            )}>
                 { children }
             </div>
         </div>
